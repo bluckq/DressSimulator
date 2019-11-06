14e975e533439e1b7a2656d71f37ebb050c5409e
Aaron <bluckq@gmail.com>
2019-11-06 14:18:12 -0800

    Initial Commit

---
+++ Dressing Simulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dressing Simulator
+//
+//  Created by Aaron on 11/6/19.
+//  Copyright © 2019 Aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
